3234d7ee0a9cada26e2911e77ce17f3e78245ca0
Wataru Okada <wataru.okada@flect.co.jp>
2022-08-14 12:05:38 +0900

    update

--- frontend/src/100_components/000_parts/005_ExportController.tsx
+++ frontend/src/100_components/000_parts/005_ExportController.tsx
@@ -27,7 +27,7 @@ export const ExportController = () => {
                 continue;
             }
             const fileName = generateWavFileName(prefix, i);
-            // zip.file(`raw/${fileName}`, blob);
+            zip.file(`raw/${fileName}`, blob);
             processedNum += 1;
             setProcessdNum(processedNum);
 
@@ -35,41 +35,41 @@ export const ExportController = () => {
             const dur = corpus.regions[i][1] - corpus.regions[i][0];
             const trimOptions = `-ss ${start} -i in.wav -t ${dur} out.wav`;
             const trimedBlob = await ffmpegState.exec(trimOptions, "in.wav", "out.wav", blob, "audio/wav");
-            // zip.file(`rawTrimed/${fileName}`, trimedBlob);
+            zip.file(`rawTrimed/${fileName}`, trimedBlob);
             processedNum += 1;
             setProcessdNum(processedNum);
 
             const options = `-i in.wav -ar 24000 out.wav`;
             const newBlob = await ffmpegState.exec(options, "in.wav", "out.wav", trimedBlob, "audio/wav");
-            // zip.file(`raw24k/${fileName}`, newBlob);
+            zip.file(`raw24k/${fileName}`, newBlob);
             processedNum += 1;
             setProcessdNum(processedNum);
         }
 
-        // for (let i = 0; i < corpus.vfWavBlob.length; i++) {
-        //     const blob = corpus.vfWavBlob[i];
-        //     if (!blob) {
-        //         continue;
-        //     }
-        //     const fileName = generateWavFileName(prefix, i);
-        //     zip.file(`vf/${fileName}`, blob);
-        //     processedNum += 1;
-        //     setProcessdNum(processedNum);
+        for (let i = 0; i < corpus.vfWavBlob.length; i++) {
+            const blob = corpus.vfWavBlob[i];
+            if (!blob) {
+                continue;
+            }
+            const fileName = generateWavFileName(prefix, i);
+            zip.file(`vf/${fileName}`, blob);
+            processedNum += 1;
+            setProcessdNum(processedNum);
 
-        //     const start = corpus.regions[i][0];
-        //     const dur = corpus.regions[i][1] - corpus.regions[i][0];
-        //     const trimOptions = `-ss ${start} -i in.wav -t ${dur} out.wav`;
-        //     const trimedBlob = await ffmpegState.exec(trimOptions, "in.wav", "out.wav", blob, "audio/wav");
-        //     zip.file(`vfTrimed/${fileName}`, trimedBlob);
-        //     processedNum += 1;
-        //     setProcessdNum(processedNum);
+            const start = corpus.regions[i][0];
+            const dur = corpus.regions[i][1] - corpus.regions[i][0];
+            const trimOptions = `-ss ${start} -i in.wav -t ${dur} out.wav`;
+            const trimedBlob = await ffmpegState.exec(trimOptions, "in.wav", "out.wav", blob, "audio/wav");
+            zip.file(`vfTrimed/${fileName}`, trimedBlob);
+            processedNum += 1;
+            setProcessdNum(processedNum);
 
-        //     const options = `-i in.wav -ar 24000 out.wav`;
-        //     const newBlob = await ffmpegState.exec(options, "in.wav", "out.wav", trimedBlob, "audio/wav");
-        //     zip.file(`vf24k/${fileName}`, newBlob);
-        //     processedNum += 1;
-        //     setProcessdNum(processedNum);
-        // }
+            const options = `-i in.wav -ar 24000 out.wav`;
+            const newBlob = await ffmpegState.exec(options, "in.wav", "out.wav", trimedBlob, "audio/wav");
+            zip.file(`vf24k/${fileName}`, newBlob);
+            processedNum += 1;
+            setProcessdNum(processedNum);
+        }
 
         setExporting(false);
         const blob = await zip.generateAsync({ type: "blob" });
