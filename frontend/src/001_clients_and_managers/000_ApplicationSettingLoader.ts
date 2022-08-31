51b3283a30ccd0ca7592c08c7377ebcf96202f99
Wataru Okada <wataru.okada@flect.co.jp>
2022-08-31 16:11:35 +0900

    update

--- frontend/src/001_clients_and_managers/000_ApplicationSettingLoader.ts
+++ frontend/src/001_clients_and_managers/000_ApplicationSettingLoader.ts
@@ -13,7 +13,7 @@ export type CorpusTextSetting = {
 }
 
 export const fetchApplicationSetting = async (): Promise<ApplicationSetting> => {
-    const url = `/api/setting`
+    const url = `./assets/setting.json`
     const res = await fetch(url, {
         method: "GET"
     });
